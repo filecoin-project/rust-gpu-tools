fe5ec5c0da4769b35ba5ae52579a767e4a7c7642
Volker Mische <volker.mische@gmail.com>
2021-04-30 14:02:57 +0200

    feat: use device name only
    There is no need to keep a copy of a device, we only need the device name.

--- src/opencl/mod.rs
+++ src/opencl/mod.rs
@@ -180,17 +180,17 @@ fn get_device_by_index(index: usize) -> Option<&'static Device> {
 }
 
 pub struct Program {
-    // TODO vmx 2021-04-11: The `Context` contains the devices, use those instead of storing them again
-    device: Device,
+    device_name: String,
     queue: CommandQueue,
     context: Context,
     kernels_by_name: HashMap<String, opencl3::kernel::Kernel>,
 }
 
 impl Program {
-    pub fn device(&self) -> Device {
-        self.device.clone()
+    pub fn device_name(&self) -> &str {
+        &self.device_name
     }
+
     pub fn from_opencl(device: Device, src: &str) -> GPUResult<Program> {
         let cached = utils::cache_path(&device, src)?;
         if std::path::Path::exists(&cached) {
@@ -210,8 +210,8 @@ impl Program {
                 .map(|kernel| (kernel.function_name().unwrap(), kernel))
                 .collect();
             let prog = Program {
+                device_name: device.name(),
                 queue,
-                device,
                 context,
                 kernels_by_name,
             };
@@ -238,7 +238,7 @@ impl Program {
             .map(|kernel| (kernel.function_name().unwrap(), kernel))
             .collect();
         Ok(Program {
-            device,
+            device_name: device.name(),
             queue,
             context,
             kernels_by_name,
